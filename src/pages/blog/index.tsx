8ac0190bd16be581cbc5256cefe942610896de31
Andrew Dunn <atd2005@gmail.com>
2023-11-11 20:41:25 -0600

    refactor blog timeline

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -7,7 +7,13 @@ import { GetStaticProps } from "next";
 import BlogPost from "../../../components/blog/blog-post";
 import Fader from "../../../components/utils/fader";
 import { withSSRContext, Predicates } from "aws-amplify";
-import { MutableRefObject, useEffect, useRef, useState } from "react";
+import {
+  MutableRefObject,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 import YearFilter from "../../../components/blog/year-filter";
 import { albertusFont } from "../../../components/bio/bio-post";
@@ -35,71 +41,158 @@ const BlogPage = ({
   const onYearSelection = async (year: string) => {
     setFeaturedPost(null);
     setCurrYear(year);
-    setCurrPage(1);
+    // setCurrPage(1);
 
-    console.log("call for models by year ", year);
-    setBlogPosts([]);
-    const models = await DataStore.query(
-      Blog,
-      (c) =>
-        c.and((c) => [
-          c.publishDate.contains(year),
-          c.status.eq(ItemStatus.ACTIVE),
-        ]),
-      {
-        page: 0,
-        limit: PAGE_LENGTH,
-        sort: (s) => s.publishDate(SortDirection.DESCENDING),
-      }
-    );
-    console.log(models);
-    setBlogPosts(models);
+    // console.log("call for models by year ", year);
+    // setBlogPosts([]);
+    // const models = await DataStore.query(
+    //   Blog,
+    //   (c) =>
+    //     c.and((c) => [
+    //       c.publishDate.contains(year),
+    //       c.status.eq(ItemStatus.ACTIVE),
+    //     ]),
+    //   {
+    //     page: 0,
+    //     limit: PAGE_LENGTH,
+    //     sort: (s) => s.publishDate(SortDirection.DESCENDING),
+    //   }
+    // );
+    // console.log(models);
+    // setBlogPosts(models);
   };
 
   const onSetFeaturedModel = (model: Blog) => {
     setFeaturedPost(model);
   };
 
-  const fetchNextPage = async (reset?: Boolean) => {
-    let pageNum;
-    if (reset) {
-      pageNum = 0;
-    } else if (currPage == 0 && blogPosts.length > 0) {
-      pageNum = 1;
-    } else {
-      pageNum = currPage;
-    }
-    console.log("fetchNextPage: ", currPage, " year: ", currYear);
-
-    const models = await DataStore.query(
-      Blog,
-      (c) =>
-        c.and((c) => [
-          c.publishDate.contains(currYear),
-          c.status.eq(ItemStatus.ACTIVE),
-        ]),
-      {
-        page: pageNum,
-        limit: PAGE_LENGTH,
-        sort: (s) => s.publishDate(SortDirection.DESCENDING),
-      }
-    );
-    setBlogPosts((prev) => [...prev, ...models]);
-    console.log("models found: ", models.length);
-    models.forEach((model, index) =>
-      console.log(index, model.title ? model.title : "untitled")
-    );
-    if (models.length == PAGE_LENGTH) {
-      setCurrPage((prev) => prev + 1);
-    } else {
-      console.log("SETTING A YEAR BACK");
-      setCurrPage(0);
-      setCurrYear((prev) => (parseInt(prev) - 1).toString());
-    }
-    if (models.length == 0) {
-      fetchNextPage(true);
-    }
+  const fetchNextPage = async () => {
+    setCurrPage((prev) => prev + 1);
   };
+
+  useEffect(() => {
+    console.log(currYear);
+  }, [currYear]);
+
+  const getYear = useCallback(() => currYear, [currYear]);
+  const setYear = useCallback((year: string) => {
+    setCurrYear(year);
+    setCurrPage(0);
+  }, []);
+
+  useEffect(() => {
+    console.log("CurrPage Effect ", currPage);
+    console.log("currentYear", getYear());
+    console.log("* * * * * * * * * * * *");
+    const getModels = async () => {
+      const models = await DataStore.query(
+        Blog,
+        (c) =>
+          c.and((c) => [
+            c.publishDate.contains(getYear()),
+            c.status.eq(ItemStatus.ACTIVE),
+          ]),
+        {
+          page: currPage,
+          limit: PAGE_LENGTH,
+          sort: (s) => s.publishDate(SortDirection.DESCENDING),
+        }
+      );
+      console.log(models);
+      setBlogPosts((prev) => [...prev, ...models]);
+      if (models.length < PAGE_LENGTH) {
+        if (parseInt(getYear()) > 2007) {
+          setYear((parseInt(getYear()) - 1).toString());
+        }
+      }
+      //setCurrPage(currPage + 1);
+    };
+
+    // setFeaturedPost(null);
+    // setCurrYear(year);
+    // setCurrPage(1);
+
+    // console.log("call for models by year ", year);
+    // setBlogPosts([]);
+    // const models = await DataStore.query(
+    //   Blog,
+    //   (c) =>
+    //     c.and((c) => [
+    //       c.publishDate.contains(year),
+    //       c.status.eq(ItemStatus.ACTIVE),
+    //     ]),
+    //   {
+    //     page: 0,
+    //     limit: PAGE_LENGTH,
+    //     sort: (s) => s.publishDate(SortDirection.DESCENDING),
+    //   }
+    // );
+    // console.log(models);
+    // setBlogPosts(models);
+    getModels();
+  }, [currPage, getYear, setYear, setBlogPosts]);
+
+  useEffect(() => {
+    console.log("Year Effect ", currYear);
+    // setFeaturedPost(null);
+    // setCurrYear(year);
+    // setCurrPage(1);
+
+    // console.log("call for models by year ", year);
+    // setBlogPosts([]);
+    // const models = await DataStore.query(
+    //   Blog,
+    //   (c) =>
+    //     c.and((c) => [
+    //       c.publishDate.contains(year),
+    //       c.status.eq(ItemStatus.ACTIVE),
+    //     ]),
+    //   {
+    //     page: 0,
+    //     limit: PAGE_LENGTH,
+    //     sort: (s) => s.publishDate(SortDirection.DESCENDING),
+    //   }
+    // );
+    // console.log(models);
+    // setBlogPosts(models);
+    // let pageNum;
+    // if (reset) {
+    //   pageNum = 0;
+    // } else if (currPage == 0 && blogPosts.length > 0) {
+    //   pageNum = 1;
+    // } else {
+    //   pageNum = currPage;
+    // }
+    // console.log("fetchNextPage: ", currPage, " year: ", currYear);
+
+    // const models = await DataStore.query(
+    //   Blog,
+    //   (c) =>
+    //     c.and((c) => [
+    //       c.publishDate.contains(currYear),
+    //       c.status.eq(ItemStatus.ACTIVE),
+    //     ]),
+    //   {
+    //     page: pageNum,
+    //     limit: PAGE_LENGTH,
+    //     sort: (s) => s.publishDate(SortDirection.DESCENDING),
+    //   }
+    // );
+    // setBlogPosts((prev) => [...prev, ...models]);
+    // console.log("models found: ", models.length);
+    // models.forEach((model, index) =>
+    //   console.log(index, model.title ? model.title : "untitled")
+    // );
+    // if (models.length == PAGE_LENGTH) {
+    //   setCurrPage((prev) => prev + 1);
+    // } else {
+    //   console.log("SETTING A YEAR BACK");
+    //   setCurrPage(0);
+    //   setCurrYear((prev) => (parseInt(prev) - 1).toString());
+    // }
+    // if (models.length == 0) {
+    //   fetchNextP
+  }, [currYear]);
 
   return (
     <div className={styles.Blog}>
